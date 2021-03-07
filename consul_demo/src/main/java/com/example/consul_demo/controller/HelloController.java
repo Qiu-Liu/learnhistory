62fe5635f82f89e1afd341cc2c5d44f43828dc2a
Qiu-Liu <822584426@qq.com>
2021-03-08 00:31:00 +0800

    集成gateway,gateway跳转失败，待修复

--- consul_demo/src/main/java/com/example/consul_demo/controller/HelloController.java
+++ consul_demo/src/main/java/com/example/consul_demo/controller/HelloController.java
@@ -1,11 +1,15 @@
 package com.example.consul_demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/consul")
 public class HelloController {
-    @RequestMapping("/hello")
+
+    @GetMapping("/hello")
     public String hello() {
         return "hello consul";
     }
